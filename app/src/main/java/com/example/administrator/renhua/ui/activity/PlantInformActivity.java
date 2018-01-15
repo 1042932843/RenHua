5b0cda8c44b19bb9bc4eb6fd2a6bc266308e0fd3
LeoC <410199842@qq.com>
2018-01-15 15:21:10 +0800

    用了U学的扫二维码控件

--- app/src/main/java/com/example/administrator/renhua/ui/activity/PlantInformActivity.java
+++ app/src/main/java/com/example/administrator/renhua/ui/activity/PlantInformActivity.java
@@ -14,11 +14,12 @@ import android.widget.Toast;
 import com.example.administrator.renhua.App;
 import com.example.administrator.renhua.R;
 import com.example.administrator.renhua.ui.view.AppWebView;
-import com.uuzuche.lib_zxing.activity.CodeUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+//import com.uuzuche.lib_zxing.activity.CodeUtils;
 
 public class PlantInformActivity extends BaseActivity {
 
@@ -91,30 +92,21 @@ public class PlantInformActivity extends BaseActivity {
 
     @OnClick(R.id.fab)
     public void fabClick() {
-        Intent intent = new Intent(PlantInformActivity.this, QRCodeActivity.class);
+        Intent intent = new Intent(PlantInformActivity.this, MipcaActivityCapture.class);
         startActivityForResult(intent, 1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1) {
-            //处理扫描结果（在界面上显示）
-            if (null != data) {
-                Bundle bundle = data.getExtras();
-                if (bundle == null) {
-                    return;
-                }
-                if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
-                    String result = bundle.getString(CodeUtils.RESULT_STRING);
-                    Toast.makeText(this, "解析结果:" + result, Toast.LENGTH_LONG).show();
-//                    Intent intent = new Intent(PlantInformActivity.this,WebViewActivity.class);
-//                    intent.putExtra("mark","plant");
-//                    intent.putExtra("url",result);
-//                    startActivity(intent);
-                } else if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_FAILED) {
-                    Toast.makeText(PlantInformActivity.this, "解析二维码失败", Toast.LENGTH_LONG).show();
-                }
-            }
+
+        if(resultCode==RESULT_OK){
+            Bundle bundle =  data.getExtras();
+            String result = bundle.getString("result");
+            Toast.makeText(PlantInformActivity.this,result,Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(PlantInformActivity.this,WebViewActivity.class);
+            intent.putExtra("mark","plant");
+            intent.putExtra("url",result);
+            startActivity(intent);
         }
 
     }
